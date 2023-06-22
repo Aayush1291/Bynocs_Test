7221150410bffb169cc25aa2ab3813b4f1b89a84
Bhumilotankar <co2021.bhumi.lotankar@ves.ac.in>
2023-06-22 11:16:50 +0530

    abc

---
+++ individual projects/Bhumi/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+
+
+function App(): JSX.Element {
+  return (
+    <Text> App</Text>
+    // by bhumi
+  );
+}
+
+
+export default App;
